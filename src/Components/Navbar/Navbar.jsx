d1f516409109e22df238f83e9db86bc780898027
Gyanesh kumar padhiary <gyaneshp.dev@gmail.com>
2023-11-02 17:57:05 +0530

    Added components to the navbar and improved navbar design

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import './Navbar.css'
 import aniverse from '../../Assets/aniverse.png'
+import aniwatchlogo from '../../Assets/aniwatchlogo.png'
+import crunchyroll from '../../Assets/crunchyroll.png'
 const Navbar = () => {
   return (
     <div className='navbar-container'>
       <div className="navbar-left">
         <img src={aniverse} alt="navbar" />
-        <input type='text' placeholder='Search'/>
+        <input className='navbar-search' type='text' placeholder='Search anime...'/>
       </div>
       <div className="navbar-middle">
-        <p>Trending</p>
         <p>Movies</p>
         <p>Tv Series</p>
+        <p>Manga</p>
       </div>
       <div className="navbar-right">
-        <a href='/'>Github</a>
-        <a href='/'>Twitter</a>
-        <a href='/'>Instagram</a>
-        <a href='/'>Linkedin</a>
+        <a className='logo1' href='https://aniwatch.to/home' target="_blank" rel="noreferrer"><img src={aniwatchlogo} alt="" /></a>
+        <a className='logo2' href='https://www.crunchyroll.com/' target="_blank" rel="noreferrer"><img src={crunchyroll} alt="" /></a>
+        
+       </div>
+       <div className="navbar-loginbtn">
         <button>Login</button>
       </div>
     </div>
