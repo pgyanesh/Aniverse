4753fe84db6fe1961e7603191da2c96abf995ac2
Gyanesh kumar padhiary <gyaneshp.dev@gmail.com>
2023-11-06 00:48:37 +0530

    Making changes to the series page

--- src/Pages/Series/Series.jsx
+++ src/Pages/Series/Series.jsx
@@ -3,18 +3,24 @@ import './Series.css'
 import SeriesItem from '../../Components/SeriesItem/SeriesItem'
 import series from '../../Assets/series'
 import './Series.css'
+import Navbar from '../../Components/Navbar/Navbar'
 const Series = () => {
   return (
+    <>
+    <Navbar/>
     <div className='series-container'>
+      <div className="series-innerContainer">
       <div className="series-head">
         <h3>TV Series</h3>
       </div>
       <div className="series-content">
         {series.map((item)=>{
-          return <SeriesItem key={item.id} title={item.title} image={item.image} rating={item.rating} link1={item.link1}/>
+          return <SeriesItem key={item.id} title={item.title} image={item.image} description={item.description?item.description.slice(0,100):" "} rating={item.rating} link1={item.link1}/>
         })}
       </div>
+      </div>
     </div>
+    </>
   )
 }
 
